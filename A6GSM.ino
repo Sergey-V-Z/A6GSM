bf88722ced522017eed655d50bb32a2754bdfb43
Sergey <skiner_1988@mail.ru>
2022-02-25 17:41:42 +0800

    First

---
+++ A6GSM.ino
@@ -0,0 +1,38 @@
+/*
+  ESP8266 Blink by Simon Peter
+  Blink the blue LED on the ESP-01 module
+  This example code is in the public domain
+
+  The blue LED on the ESP-01 module is connected to GPIO1
+  (which is also the TXD pin; so we cannot use Serial.print() at the same time)
+
+  Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
+*/
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  Serial.println("start");
+  Serial.begin(115200);
+  Serial.println("115200 set");
+  Serial.swap();
+  Serial.println("swap");
+  Serial.flush();
+  Serial1.begin(115200);
+  Serial1.println("serial1_start");
+  Serial1.setDebugOutput(true);
+  Serial1.println("serial1_debug true");
+
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  // but actually the LED is on; this is because
+  // it is active low on the ESP-01)
+  delay(1000);                      // Wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+  Serial.println("loop");
+  Serial1                                                                                                                    .println("loop");
+}
